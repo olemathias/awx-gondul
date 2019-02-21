86d51f84b424daec8f3f56604f9c63e4dc2c69ff
Ole Mathias Aa. Heggem <olemathias.aa.heggem@gmail.com>
2019-02-21 10:04:23 +0100

    Use env vars in gondul.py

--- gondul.py
+++ gondul.py
@@ -3,6 +3,8 @@
 import argparse
 import json
 import requests
+import os
+from requests.auth import HTTPBasicAuth
 
 def parse_args():
     '''
@@ -20,59 +22,71 @@ def parse_args():
     return parser.parse_args()
 
 def list_switches():
-    r = requests.get('http://10.10.2.20/api/read/switches-management')
+    r = requests.get('{}/api/read/switches-management'.format(os.environ['GONDUL_URL']), auth=HTTPBasicAuth(os.environ['GONDUL_USER'], os.environ['GONDUL_PASSWORD']))
     switches = r.json()['switches']
+    r = requests.get('{}/api/public/switches'.format(os.environ['GONDUL_URL']), auth=HTTPBasicAuth(os.environ['GONDUL_USER'], os.environ['GONDUL_PASSWORD']))
+    public_switches = r.json()['switches']
+
     hostsvars = {}
     hosts = []
+
+    inventory = {}
+
     for switch_name in switches:
         switch = switches[switch_name]
+        switch.update(public_switches[switch_name])
         hostvar = {
             'distro_name': switch['distro_name'],
             'distro_phy_port': switch['distro_phy_port'],
-            'subnet4': switch['subnet4'],
-            'subnet6': switch['subnet6'],
             'community': switch['community'],
             'sysname': switch['sysname'],
             'mgmt_v4_addr': switch['mgmt_v4_addr'],
             'mgmt_v6_addr': switch['mgmt_v6_addr'],
             'traffic_vlan': switch['traffic_vlan'],
             'ansible_host': switch['mgmt_v4_addr'],
-            'ansible_network_os': 'junos',
             'host': switch['mgmt_v4_addr']
         }
+        for tag in switch['tags']:
+            if tag not in inventory:
+                inventory[tag] = {'hosts': []}
+            inventory[tag]['hosts'].append(switch['sysname'])
         hostsvars.update({switch['sysname']: hostvar})
         hosts.append(switch['sysname'])
 
-    inventory = {
-    "_meta": hostsvars,
-    "switches": {"hosts": hosts}
-    }
+    inventory.update({
+    '_meta': hostsvars,
+    'switches': {'hosts': hosts},
+    })
 
     inv_string = json.dumps(inventory, indent=1, sort_keys=True)
     print(inv_string)
 
 def get_host_attributes(host):
-    r = requests.get('http://10.10.2.20/api/read/switches-management')
+    r = requests.get('{}/api/read/switches-management'.format(os.environ['GONDUL_URL']), auth=HTTPBasicAuth(os.environ['GONDUL_USER'], os.environ['GONDUL_PASSWORD']))
     switches = r.json()['switches']
+    r = requests.get('{}/api/public/switches'.format(os.environ['GONDUL_URL']), auth=HTTPBasicAuth(os.environ['GONDUL_USER'], os.environ['GONDUL_PASSWORD']))
+    public_switches = r.json()['switches']
+
     if host not in switches:
         print(json.dumps({}, indent=1, sort_keys=True))
         return
 
     switch = switches[host]
+    switch.update(public_switches[host])
     hostvar = {
         'distro_name': switch['distro_name'],
         'distro_phy_port': switch['distro_phy_port'],
-        'subnet4': switch['subnet4'],
-        'subnet6': switch['subnet6'],
         'community': switch['community'],
         'sysname': switch['sysname'],
         'mgmt_v4_addr': switch['mgmt_v4_addr'],
         'mgmt_v6_addr': switch['mgmt_v6_addr'],
         'traffic_vlan': switch['traffic_vlan'],
         'ansible_host': switch['mgmt_v4_addr'],
-        'ansible_network_os': 'junos',
-        'host': switch['mgmt_v4_addr']
+        'host': switch['mgmt_v4_addr'],
+        'tags': switch['tags']
     }
+    if 'junos' in switch['tags']:
+        hostvar['ansible_network_os'] = 'junos'
     inv_string = json.dumps(hostvar, indent=1, sort_keys=True)
     print(inv_string)
 
